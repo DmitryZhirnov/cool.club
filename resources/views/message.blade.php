ffc82a0fd00d0354f77afb631ce8c53bfab2754f
dmitry <dmzhirnov@mail.ru>
2020-09-17 14:28:19 +0300

    Установил зависимости npm и добавил немного стилей

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -4,22 +4,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css">
     <title></title>
 </head>
 
 <body>
-    <form method="POST" action="/send">
+    <form method="POST" action="/send" class="message-form">
         @csrf
-        <label for="message_content">Введите сообщение</label>
-        <textarea name="message_content" cols="30" rows="10"></textarea>
+        <label for="message_content" class="message-form__content-label">Введите сообщение</label>
+        <textarea name="message_content" cols="30" rows="10" class="message-form__content"></textarea>
+        <button type="submit" class="message-form__submit">Отправить</button>
         @error('message_content')
-        <div class="alert alert-danger">{{ $message }}</div>
+        <div class="message-form__errors">{{ $message }}</div>
         @enderror
-        <button type="submit">Отправить</button>
+        @if (Session::has('success'))
+        <div class="message-form__info">{{ Session::get('success') }}</div>
+        @endif
     </form>
-    @if (Session::has('success'))
-    <div class="alert alert-info">{{ Session::get('success') }}</div>
-    @endif
+
 </body>
 
 </html>
