9bf127c2e6968e72fa5f37dc2173574e0a44fa17
dmitry <dmzhirnov@mail.ru>
2020-09-17 11:28:36 +0300

    Добавил валидацию сообщения и маршрут для записи .

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\MessageRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Cookie;
@@ -17,5 +18,13 @@ class MessageController extends Controller
     {
         $senderCookie = Cookie::has('sender_id') ? Cookie::get('sender_id') : cookie('sender_id', Str::random(10), 0);
         return (new Response(view('message')))->withCookie($senderCookie);
+    }
+
+    /**
+     * Отправка сообщения по e-mail
+     * и запись его в БД
+     */
+    public function send(MessageRequest $request)
+    {
     }
 }
